94b8898c234f3f84f3ebca8101a7d6a98bf7888d
Eddie Hsu <aoe102198@gmail.com>
2019-06-11 23:02:58 +0800

    add #02 Python native data structures
    add number type

---
+++ 02 - Environment and CLI setup/Number.py
@@ -0,0 +1,14 @@
+a = 5
+print(a, "is of type", type(a))
+
+a = 2.0
+print(a, "is of type", type(a))
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
+# Complex numbers are written in the form, x + yj, 
+# where x is the real part and y is the imaginary part.
+
+# We can use the 'type()' function to know which class a variable or a value belongs to
+# and the 'isinstance()' function to check if an object belongs to a particular class.
+

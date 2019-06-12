407a83b2c3e00ca0ce7603158f3c4a659c575d50
Eddie Hsu <aoe102198@gmail.com>
2019-06-12 23:22:25 +0800

    add #02 Python native data structures
    add String（字符串),  List（列表）

---
+++ 02 - Environment and CLI setup/List.py
@@ -0,0 +1,20 @@
+a = [5,10,15,20,25,30,35,40]
+
+# a[2] = 15
+print("a[2] = ", a[2])
+
+# a[0:3] = [5, 10, 15]
+print("a[0:3] = ", a[0:3])
+
+# a[5:] = [30, 35, 40]
+print("a[5:] = ", a[5:])
+
+a = [1, 2.2, 'python']
+print("a[2:] = ", a[2])
+
+
+# Lists are mutable, meaning, value of elements of a list can be altered.
+x = [1,2,3]
+x[2]=4
+x
+# [1, 2, 4]
---
+++ 02 - Environment and CLI setup/String.py
@@ -0,0 +1,18 @@
+s = 'Hello world!'
+
+# s[4] = 'o'
+print("s[4] = ", s[4])
+
+# s[6:11] = 'world'
+print("s[6:11] = ", s[6:11])
+
+# Generates error
+# Strings are immutable in Python
+s[5] ='d'
+
+# Like list and tuple, slicing operator [ ] can be used with string. Strings are immutable.
+# String is sequence of Unicode characters. 
+# We can use single quotes or double quotes to represent strings. 
+# Multi-line strings can be denoted using triple quotes, ''' or """.
+
+

7b7950bb04aaae2101f060e63a7813ffc8a0151d
Eddie Hsu <aoe102198@gmail.com>
2019-06-13 23:45:17 +0800

    add #02 Python native data structures
    add Set and Tuple

---
+++ 02 - Environment and CLI setup/Set.py
@@ -0,0 +1,16 @@
+
+a = {5,2,3,1,4}
+
+# printing set variable
+print("a = ", a)
+
+# data type of variable a
+print(type(a))
+
+
+# Set is an unordered collection of unique items. 
+# Set have unique values. They eliminate duplicates.
+
+x = {1,2,2,3,3,3}
+x
+# {1, 2, 3}

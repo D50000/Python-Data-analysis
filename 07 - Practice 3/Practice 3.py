b1f68fd27518d05f90c7068f6e888e4db1d08dda
Eddie Hsu <aoe102198@gmail.com>
2019-09-02 01:11:36 +0800

    update #07 code

--- 07 - Practice 3/Practice 3.py
+++ 07 - Practice 3/Practice 3.py
@@ -1,8 +1,6 @@
 # 3.練習載入定位符分隔的檔案data1.tab，載入過程與逗號分隔檔案.csv完全相同。
 
 
-
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -16,4 +14,4 @@ Created on Mon May 20 19:35:32 2019
 
 #import library
 import pandas as pd
-dat = pd.read_csv('/Users/eddiehsu/Developer/GitHub/temp/data.tab', sep = '\ ')
+dat = pd.read_csv('/Users/eddiehsu/Developer/GitHub/Python-Data-analysis/07 - Practice 3/data1.tab', sep = '\ ')

3dcc6a65eb196685672040b18b78de3533ca2c9a
Eddie Hsu <aoe102198@gmail.com>
2019-09-02 23:04:09 +0800

    add #08
    Practice 4

---
+++ 08 - Practice 4/Practice 4.py
@@ -0,0 +1,24 @@
+# 4.分別創建兩個串列內含三個整數(假設分別為1, 2, 3和4, 5, 6)，將其用二元的加號運算子進行運算。
+
+
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 20 19:35:47 2019
+
+@author: eddiehsu
+"""
+
+
+### Practice 4
+
+#import library
+import pandas as pd
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+# list appending
+type(a)
+c = a + b
